0d24bbefa37ef7d8d01de809d1236895d79f70ad
arshad31 <arshad.cse.sub48@gmail.com>
2022-05-25 16:52:12 +0600

    refresh page done

--- webFeaturesUsingPOM/src/test/java/pageObjectPackage/PO_004_Refresh.java
+++ webFeaturesUsingPOM/src/test/java/pageObjectPackage/PO_004_Refresh.java
@@ -18,10 +18,13 @@ public class PO_004_Refresh {
     String url ="https://www.google.com";
 
     //Method to perform action in the page
-    public void refresh() { 
+    public void refresh() throws InterruptedException {
         driver.navigate().to(url);
+        Thread.sleep(2000);
         driver.navigate().back();
+        Thread.sleep(2000);
         driver.navigate().forward();
+        Thread.sleep(2000);
         driver.navigate().refresh();
     }
 
